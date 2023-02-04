48bd5cb28bed2ddb8a9e6ead57b6492763ed3054
Alexis Bouchez <bouchezalexis1@gmail.com>
2023-02-04 22:48:11 +0100

    feat: switch from MinIO to Cloudinary

--- src/common/file-manager.service.ts
+++ src/common/file-manager.service.ts
@@ -1,53 +1,38 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import * as Minio from 'minio'
+import { Injectable, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { v2 as cloudinary } from "cloudinary";
 
 @Injectable()
-export class FileManagerService implements OnModuleInit {
-  private readonly minioClient: Minio.Client
-  private readonly logger = new Logger(FileManagerService.name)
-  private static initialized = false
-
-  constructor(private readonly configService: ConfigService) {
-    this.minioClient = new Minio.Client({
-      endPoint: this.configService.get('MINIO_ENDPOINT'),
-      port: Number(this.configService.get('MINIO_PORT')),
-      useSSL: this.configService.get('MINIO_USE_SSL') === 'true',
-      accessKey: this.configService.get('MINIO_ROOT_USER') || this.configService.get('MINIO_ACCESS_KEY'),
-      secretKey: this.configService.get('MINIO_ROOT_PASSWORD') || this.configService.get('MINIO_SECRET_KEY'),
-    })
-  }
-
-  async onModuleInit() {
-    if (FileManagerService.initialized) return
-
-    this.logger.log('Initializing MinIO...')
-    await this.createBucketIfNotExists('covers')
-    await this.createBucketIfNotExists('photos')
-    await this.createBucketIfNotExists('celebrities')
-    this.logger.log('MinIO initialized')
-
-    FileManagerService.initialized = true
-  }
-
-  async createBucketIfNotExists(bucketName: string) {
-    const bucketExists = await this.minioClient.bucketExists(bucketName)
-    if (!bucketExists) {
-      await this.minioClient.makeBucket(bucketName, 'eu-west-1')
-    }
+export class FileManagerService {
+  constructor(configService: ConfigService) {
+    cloudinary.config({
+      cloud_name: configService.get("CLOUDINARY_CLOUD_NAME"),
+      api_key: configService.get("CLOUDINARY_API_KEY"),
+      api_secret: configService.get("CLOUDINARY_API_SECRET"),
+    });
   }
 
   async uploadFile(bucketName: string, file: Express.Multer.File) {
-    const fileName = `${Date.now()}-${file.originalname}`
-    await this.minioClient.putObject(bucketName, fileName, file.buffer, file.size)
-    return fileName
+    const fileName = `${Date.now()}-${file.originalname}`;
+    await cloudinary.uploader.upload(fileName, {
+      folder: bucketName,
+      use_filename: true,
+      unique_filename: false,
+    });
+
+    return fileName;
   }
 
   async getFileUrl(bucketName: string, fileName: string) {
-    return await this.minioClient.presignedUrl('GET', bucketName, fileName)
+    return cloudinary.url(fileName, {
+      secure: true,
+      folder: bucketName,
+      type: "upload",
+      resource_type: "image",
+    });
   }
 
   async deleteFile(bucketName: string, fileName: string) {
-    await this.minioClient.removeObject(bucketName, fileName)
+    await cloudinary.uploader.destroy(fileName);
   }
 }
